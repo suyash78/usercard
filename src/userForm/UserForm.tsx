441c5c2fda0a97a7675c44c43b1bfd15cd8e461f
Suyash <suyashparate78@gmail.com>
2021-08-24 19:59:35 +0530

    User List component complete ( Logically )

--- src/userForm/UserForm.tsx
+++ src/userForm/UserForm.tsx
@@ -1,39 +1,78 @@
 import React , { useState } from 'react';
+import UserList from '../userList/UserList';
+
+interface UserData {
+    id: number | undefined;
+    name: string | undefined;
+    age: number | undefined;
+    gender: string | undefined;
+}
 
 export default function UserForm() {
 
-    const [userInfo, setUserInfo] = useState({ userId : 0 , name : '' , age : 0 , gender : '' });
-    const [userList, setUserList] = useState<Array<object>>([]);
+    const [userInfo, setUserInfo] = useState<UserData>({ id : 0 , name : '' , age : 0 , gender : '' });
+    const [userInfoList, setUserInfoList] = useState<Array<UserData>>([]);
+    const [userIndex, setUserIndex] = useState<number>(0);
+    const [isUpdate, setIsUpdate] = useState<number>(0)
 
-    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event : React.MouseEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setUserList([...userList , userInfo]);
-        setUserInfo({ userId : (userList.length + 1) , name : '' , age : 0 , gender : '' });
+        if(userInfoList.length >= (userIndex + 1)) {
+            userInfoList[(userIndex)] = userInfo;
+            setIsUpdate(0);
+        } else {
+            setUserInfoList([...userInfoList , userInfo]);
+            setUserIndex(userIndex + 1);
+        }
+        setUserInfo({ id : (userInfoList.length + 1) , name : '' , age : 0 , gender : '' });
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name , value } = event.target;
-        console.log(name, value);
         setUserInfo({...userInfo , [name] : value });
     }
-
-    console.log(userList);
     
     return (
         <>
-            <div className="info_wrap">
-                <div className="info_card">
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" className="info_input" value={userInfo.name} onChange={handleChange} name="name" placeholder="Name" />
-                        <input type="number" className="info_input" value={userInfo.age} onChange={handleChange} name="age" placeholder="Age" />
-                        <div className="info_radio_wrap">
-                            <div><input type="radio" name="gender" onChange={handleChange} id="gender_male" value="Male" /><label htmlFor="gender_male"> Male</label></div>
-                            <div><input type="radio" name="gender" onChange={handleChange} id="gender_female" value="Female" /><label htmlFor="gender_female"> Female</label></div>
-                            <div><input type="radio" name="gender" onChange={handleChange} id="gender_others" value="Others" /><label htmlFor="gender_others"> Others</label></div>
-                        </div>
-                        <button type="submit" className="info_btn">Add</button>
-                    </form>
+            <div className="main-wrap">
+
+                {/* User Form component */}
+                <div className="info_wrap">
+                    <div className="info_card">
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" className="info_input" value={userInfo.name} onChange={handleChange} name="name" placeholder="Name" autoFocus />
+                            <input type="number" className="info_input" value={userInfo.age} onChange={handleChange} name="age" placeholder="Age" />
+                            <div className="info_radio_wrap">
+                                <div><input type="radio" name="gender" onChange={handleChange} id="gender_male" value="Male" /><label htmlFor="gender_male"> Male</label></div>
+                                <div><input type="radio" name="gender" onChange={handleChange} id="gender_female" value="Female" /><label htmlFor="gender_female"> Female</label></div>
+                                <div><input type="radio" name="gender" onChange={handleChange} id="gender_others" value="Others" /><label htmlFor="gender_others"> Others</label></div>
+                            </div>
+                            <button type="submit" className="info_btn">{(isUpdate == 0) ? "Add" : "Update"}</button>
+                        </form>
+                    </div>
                 </div>
+
+                {/* User Cards List component */}
+                <div className="userList-card-wrap">
+
+                    {userInfoList.map( (userRef , mapIndex) => {
+                        return(
+                            <div key={userRef.id}>
+                                <UserList userName={userRef.name} userAge={userRef.age} UserGender={userRef.gender} 
+                                    deleteClick={() => setUserInfoList( userInfoList.filter( (arrEel , index) => index!==mapIndex ))} 
+                                    updateClick={() => {
+                                        let findUser: UserData | undefined = userInfoList.find( user => user.id === userRef.id );
+                                        setUserInfo({id: findUser?.id , name: findUser?.name , age: findUser?.age , gender: findUser?.gender })
+                                        setUserIndex(mapIndex);
+                                        setIsUpdate(1);
+                                    }} />
+                            </div>
+                            
+                        );
+                    })}
+                    
+                </div>
+                
             </div>
         </>
     );
--- src/userList/UserList.tsx
+++ src/userList/UserList.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-export default function UserList() {
+interface Props {
+    userName: string | undefined;
+    userAge: number | undefined;
+    UserGender: string | undefined;
+    deleteClick: React.MouseEventHandler;
+    updateClick: React.MouseEventHandler;
+}
+
+const UserList : React.FC<Props> = ({ userName , userAge , UserGender , deleteClick , updateClick }) => {
     return (
-        <div>
-            
+        <div className="userList-card">
+            <div className="userList-card-userName">Name : {userName}</div>|
+            <div className="userList-card-userAge">Age : {userAge}</div>|
+            <div className="userList-card-userGender">Gender : {UserGender}</div>
+            <div className="userList-card-btnWrap">
+                <button onClick={deleteClick} className="userList-card-deleteBtn">Delete</button>
+                <button onClick={updateClick} className="cardList-card-updateBtn">Update</button>
+            </div>
         </div>
     );
 }
+
+export default UserList;
